15d584e821e02fead9942fc92d7a0acfc9ff1173
Kyrellos Saad Ibrahim <kyrellosibrahim@gmail.com>
2026-01-29 15:08:26 -0600

    fruit follow player and instantation
    fruit follows the player movement and pressing spacebar drops the fruit. New fruit appears when one is dropped.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,6 +4,9 @@ using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float speed;
+    public GameObject fruit;
+    private GameObject currentFruit;
+    public float fruitOffsetY = -0.6f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +16,28 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(currentFruit != null) {
+            // current player position
+            Vector3 playerPos = transform.position;
+            Vector3 fruitOffset = new Vector3(0.0f, fruitOffsetY, 0.0f);
+            currentFruit.transform.position = playerPos + fruitOffset;
+        }
+        else {
+            // make player hold a new fruit
+            currentFruit = Instantiate(fruit, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        }
         Keyboard k = Keyboard.current;
+        if(k.spaceKey.wasPressedThisFrame) {
+            Rigidbody2D body = currentFruit.GetComponent<Rigidbody2D>();
+            body.gravityScale = 1.0f;
+
+            Collider2D collider = currentFruit.GetComponent<Collider2D>();
+            collider.enabled = true;
+            
+            currentFruit = null;
+
+        }
         if(k.leftArrowKey.isPressed || k.aKey.isPressed)
         {
             Vector3 newPos = transform.position;
